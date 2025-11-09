aa7ccf30f96b18d3bb9a45e4ceb90c29aff62323
jesús david barrera montes <jesusdavidbm@ufps.edu.co>
2025-11-09 14:03:54 -0500

    Integración de los PQRS con el backend

--- backend/src/main/java/com/ufps/Universal/Latin/De/Servicios/S/A/S/repository/ClienteRepository.java
+++ backend/src/main/java/com/ufps/Universal/Latin/De/Servicios/S/A/S/repository/ClienteRepository.java
@@ -1,10 +1,13 @@
 package com.ufps.Universal.Latin.De.Servicios.S.A.S.repository;
 
-import com.ufps.Universal.Latin.De.Servicios.S.A.S.model.Cliente;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ufps.Universal.Latin.De.Servicios.S.A.S.model.Cliente;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    // Puedes agregar métodos personalizados aquí si lo requieres
+    Optional<Cliente> findByEmail(String email);
 }
